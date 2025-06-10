540c3dcb268889b53bead7fc4365aa095adcc3d9
GArysTrousers <ben.m.lee@outlook.com>
2025-06-11 09:29:04 +1000

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Setup Sveltkit Project
+## Install nvm
+```
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+bash
+nvm install v24
+```
+## Install git
+```
+sudo apt install git
+```
+## Setup Server
+```
+setup-sveltekit-project.sh | bash
+sh edit.sh
+```
---
+++ setup-sveltekit-project.sh
@@ -0,0 +1,21 @@
+mkdir ./data
+mkdir ./site
+mkdir ./env
+mkdir ./logs
+
+touch ./env/.env
+
+# make edit script
+cat > ./edit.sh << EOF
+Put the script here
+EOF
+
+# make update.sh
+cat > ./update.sh << EOF
+Put the script here
+EOF
+
+# make ecosystem.config.js
+cat > ./ecosystem.config.js << EOF
+Put the script here
+EOF
