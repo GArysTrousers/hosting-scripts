8f22faae29507039750aad6b38ad66d0625ac8fa
Ben Lee <43531407+GArysTrousers@users.noreply.github.com>
2026-02-04 09:58:15 +1100

    Update setup-sveltekit-project.sh

--- setup-sveltekit-project.sh
+++ setup-sveltekit-project.sh
@@ -7,15 +7,46 @@ touch ./env/.env
 
 # make edit script
 cat > ./edit.sh << EOF
-Put the script here
+nano ./update.sh
+nano ./ecosystem.config.js
 EOF
 
 # make update.sh
 cat > ./update.sh << EOF
-Put the script here
+#!/bin/bash
+
+name=app_name
+repo=github_repo_name
+token="github token"
+dir="/home/server"
+
+pm2 stop $name
+
+set -e
+
+rm -rf "$dir/site"
+
+git clone https://${token}@github.com/GArysTrousers/${repo}.git "$dir/site"
+cp "$dir/env/.env" "$dir/site"
+cd "$dir/site"
+npm i
+npm run build
+
+pm2 start $name
 EOF
 
 # make ecosystem.config.js
 cat > ./ecosystem.config.js << EOF
-Put the script here
+module.exports = {
+  apps: [
+    {
+      name: "app_name",
+      script: "./index.js",
+      cwd: "/home/server/site/build/",
+      env: {
+        PORT: "3000"
+      }
+    }
+  ]
+}
 EOF
